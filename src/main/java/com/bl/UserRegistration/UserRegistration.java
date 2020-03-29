cf4136edfc578b6f86be3404e5630d93e2f919c6
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-29 17:23:24 +0530

    REFACTOR: Test Case Naming Convention

--- src/main/java/com/bl/UserRegistration/UserRegistration.java
+++ src/main/java/com/bl/UserRegistration/UserRegistration.java
@@ -5,7 +5,7 @@ public class UserRegistration {
     private static final String LAST_NAME_PATTERN = "^[A-Z][a-z]{2,}$";
     private static final String EMAIL_ID_PATTERN = "^([a-zA-Z0-9]{1,}[._+-]?[0-9a-zA-Z]{1,})[@][a-zA-Z0-9]{1,}([.][a-zA-Z]{2,3}){1,2}$";
     private static final String MOBILE_NUMBER_PATTERN = "^[0-9]{2,3}[ ][0-9]{10}$";
-    private static final String PASSWORD = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+    private static final String PASSWORD = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9@#$%&*]{8,}$";
 
 
     public static boolean checkFirstName(String firstName) {
