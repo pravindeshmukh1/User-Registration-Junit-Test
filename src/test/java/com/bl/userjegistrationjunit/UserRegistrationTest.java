1c587983ef4bc55850414a826d6599173b25e493
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-29 15:01:44 +0530

    REFACTOR: Test case name

--- src/test/java/com/bl/userjegistrationjunit/UserRegistrationTest.java
+++ src/test/java/com/bl/userjegistrationjunit/UserRegistrationTest.java
@@ -6,22 +6,22 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    public void whenGivenFirstName_withThreeOrMoreCharacter_and_firstCharacterCapital_shouldReturnTrue() {
+    public void givenFirstName_withThreeOrMoreCharacter_and_firstCharacterCapital_shouldReturnTrue() {
         Assert.assertTrue(UserRegistration.checkFirstName("Pravin"));
     }
 
     @Test
-    public void whenGivenFirstName_lessThenThreeCharacter_shouldReturnFalse() {
+    public void givenFirstName_lessThenThreeCharacter_shouldReturnFalse() {
         Assert.assertFalse(UserRegistration.checkFirstName("pr"));
     }
 
     @Test
-    public void whenGivenFirstName_onlyLowerCaseCharacter_shouldReturnFalse() {
+    public void givenFirstName_onlyLowerCaseCharacter_shouldReturnFalse() {
         Assert.assertFalse(UserRegistration.checkFirstName("pravin"));
     }
 
     @Test
-    public void whenGivenFirstName_withThreeOrMoreCharacter_and_numberOrSpecialCharacter_shouldReturnFalse() {
+    public void givenFirstName_withThreeOrMoreCharacter_and_numberOrSpecialCharacter_shouldReturnFalse() {
         Assert.assertFalse(UserRegistration.checkFirstName("Pravin1@"));
     }
 
